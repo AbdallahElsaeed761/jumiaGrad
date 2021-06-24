57093ad6e28f5bd23f37ac9ad17c39997f6289bb
AbdallahElsaeed761 <77922312+AbdallahElsaeed761@users.noreply.github.com>
2021-06-24 23:27:08 +0200

    Rigister Controller and some Lib Required

--- JumiaProject/JumiaWebApiGrad/Controllers/AccountController.cs
+++ JumiaProject/JumiaWebApiGrad/Controllers/AccountController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BL.AppServices;
+using BL.StatisClass;
+using BL.ViewModels;
+using DAL.Models;
+using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,10 +15,56 @@ namespace JumiaWebApiGrad.Controllers
 {
     public class AccountController : ApiController
     {
-        //[HttpPost]
-        //public IHttpActionResult Registertion()
-        //{
+        private AccountAppServices _accountAppService;
+        private CartAppServices _cartAppService;
+        private ViewsAppServices _viewAppService;
+        private RoleAppServices _roleAppService;
+        IHttpContextAccessor _httpContextAccessor;
+        public AccountController(
+             AccountAppServices accountAppService,
+            CartAppServices cartAppService,
+            ViewsAppServices viewAppService,
+            RoleAppServices roleAppService,
+            IHttpContextAccessor httpContextAccessor
+            )
+        {
+            _accountAppService = accountAppService;
+            _cartAppService = cartAppService;
+            _viewAppService = viewAppService;
+            _roleAppService = roleAppService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        [HttpPost]
+        public IHttpActionResult RegisterUser([FromBody] RegisterViewModel model)
+        {
 
-        //}
+            return  Register(model, UserRoles.Customer);
+
+        }
+        private IHttpActionResult Register(RegisterViewModel model, string roleName)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result =  _accountAppService.Register(model);
+
+            if (!result.Succeeded)
+                return StatusCode(HttpStatusCode.InternalServerError);
+
+            applicationUser identityUser =  _accountAppService.Find(model.UserName, model.Password);
+            #region oldCreateCartWishlist
+            ////create cart for new user
+            //CartViewModel cartViewModel = new CartViewModel() { ApplicationUserIdentity_Id = identityUser.Id };
+            //_cartAppService.SaveNewCart(cartViewModel);
+
+            //WishlistViewModel wishlistViewModel = new WishlistViewModel() { ApplicationUserIdentity_Id = identityUser.Id };
+            //_wishlistAppService.SaveNewWishlist(wishlistViewModel);
+            #endregion
+            //create roles
+            //await _roleAppService.CreateRoles();
+             _accountAppService.AssignToRole(identityUser.Id, roleName);
+            return Ok( "User created successfully!");
+
+        }
     }
 }
---
+++ JumiaProject/JumiaWebApiGrad/Controllers/BrandController.cs
@@ -0,0 +1,35 @@
+﻿using BL.AppServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JumiaWebApiGrad.Controllers
+{
+    public class BrandController : ApiController
+    {
+         BrandsAppServices _brandsAppServices;
+        public BrandController()
+        {
+
+        }
+
+        public BrandController(BrandsAppServices brandsAppServices)
+        {
+            _brandsAppServices = brandsAppServices;
+        }
+        public IHttpActionResult GetAllBrand()
+        {
+            return Ok(_brandsAppServices.GetAllBrands());
+        }
+        public IHttpActionResult GetBrandById(int id)
+        {
+            var result = _brandsAppServices.GetBrand(id);
+            if (result == null)
+                return NotFound();
+            return Ok();
+        }
+    }
+}
